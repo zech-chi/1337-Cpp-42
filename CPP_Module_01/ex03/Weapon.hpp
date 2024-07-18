855915936e4f71c74d7f14a2530e90cb06011111
zech-chi <machinelearning1.618@gmail.com>
2024-07-18 14:51:20 +0100

    done cpp_01/ex03

--- CPP_Module_01/ex03/Weapon.hpp
+++ CPP_Module_01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 10:20:36 by zech-chi          #+#    #+#             */
-/*   Updated: 2024/07/17 15:36:32 by zech-chi         ###   ########.fr       */
+/*   Updated: 2024/07/18 14:50:50 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ class Weapon {
 		~Weapon( void );
 
 		const std::string&	getType ( void ) const;
-		void				setType(const std::string& newType );
+		void				setType( const std::string& newType );
 
 	private:
 		std::string	type;
