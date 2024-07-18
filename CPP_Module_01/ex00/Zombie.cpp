c2321dc25d17c9cab978a31324744ef7df5d506b
zech-chi <machinelearning1.618@gmail.com>
2024-07-18 13:54:15 +0100

    done cpp_01/ex00

--- CPP_Module_01/ex00/Zombie.cpp
+++ CPP_Module_01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 21:24:04 by zech-chi          #+#    #+#             */
-/*   Updated: 2024/07/18 13:44:40 by zech-chi         ###   ########.fr       */
+/*   Updated: 2024/07/18 13:51:26 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 Zombie::Zombie( const std::string newName )
 {
-	std::cout << YELLOW << "constructor called" << RESET << std::endl;
+	std::cout << YELLOW << "constructor called for zombie: " << newName << RESET << std::endl;
 	name = newName;
 }
 
 Zombie::~Zombie(void)
 {
-	std::cout << ORANGE << "destructor called" << RESET << std::endl;
+	std::cout << ORANGE << "destructor called for zombie: " << name << RESET << std::endl;
 }
 
 void	Zombie::announce(void)
