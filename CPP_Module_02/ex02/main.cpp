fbce907d983e2f3f32d95c12cff0116de584c6d2
zech-chi <machinelearning1.618@gmail.com>
2024-07-22 13:37:57 +0100

    update cpp02 ex02

--- CPP_Module_02/ex02/main.cpp
+++ CPP_Module_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 12:44:19 by zech-chi          #+#    #+#             */
-/*   Updated: 2024/07/21 18:30:39 by zech-chi         ###   ########.fr       */
+/*   Updated: 2024/07/22 13:36:25 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
+
 	std::cout << b << std::endl;
+
 	std::cout << Fixed::max( a, b ) << std::endl;
+
 	return 0;
 }
