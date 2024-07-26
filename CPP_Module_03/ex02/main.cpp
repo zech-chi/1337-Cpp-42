281bac31c294aa9d01aa5df69c83035b9b7ba815
zech-chi <machinelearning1.618@gmail.com>
2024-07-26 14:03:39 +0100

    update cpp03/ex02

--- CPP_Module_03/ex02/main.cpp
+++ CPP_Module_03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 21:49:29 by zech-chi          #+#    #+#             */
-/*   Updated: 2024/07/24 11:40:12 by zech-chi         ###   ########.fr       */
+/*   Updated: 2024/07/26 14:00:43 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,16 @@ int main()
     FragTrap    f1("fragoza");
     FragTrap    f2;
 
-    f2 = f1;
-
-    for (int i = 1; i < 100; i++)
+    f1.display();
+    f2.display();
+    for (int i = 1; i <= 105; i++)
     {
         std::cout << i << "  ";
-        f1.attack("titaz");
+        f1.attack("fragtrap0");
+        f2.takeDamage(f1.getAttackDamage());
     }
+    f1.display();
+    f2.display();
     f1.highFivesGuys();
     return (0);
 }
