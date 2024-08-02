cc6422de1ab12c2ad2d1c3046cbb44092845c302
zech-chi <machinelearning1.618@gmail.com>
2024-08-02 19:00:36 +0100

    cpp04/ex00

--- CPP_Module_04/ex00/main.cpp
+++ CPP_Module_04/ex00/main.cpp
@@ -6,32 +6,30 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 16:34:37 by zech-chi          #+#    #+#             */
-/*   Updated: 2024/08/02 18:46:26 by zech-chi         ###   ########.fr       */
+/*   Updated: 2024/08/02 19:00:06 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
-#include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
-// int main()
-// {
-//     const Animal* meta = new Animal();
-//     const Animal* j = new Dog();
-//     const Animal* i = new Cat();
-//     std::cout << j->getType() << " " << std::endl;
-//     std::cout << i->getType() << " " << std::endl;
-//     i->makeSound(); //will output the cat sound!
-//     j->makeSound(); //will output the dog sound!
-//     meta->makeSound();
+int main()
+{
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound(); //will output the dog sound!
+    meta->makeSound();
 
-//     delete meta;
-//     delete j;
-//     delete i;
-//     return 0;
-// }
+    delete meta;
+    delete j;
+    delete i;
+    return 0;
+}
 
 // int main()
 // {
@@ -46,15 +44,14 @@
 //     return 0;
 // }
 
+// int main()
+// {
+//     WrongCat  c1 = WrongCat();
+//     WrongCat  c2 = c1;
+//     WrongCat c3;
 
-int main()
-{
-    Cat  c1 = Cat();
-    Cat  c2 = c1;
-    Cat c3;
-
-    c3 = c1;
-    std::cout << c1.getType() << std::endl;
-    std::cout << c2.getType() << std::endl;
-    std::cout << c3.getType() << std::endl;
-}
+//     c3 = c1;
+//     std::cout << c1.getType() << std::endl;
+//     std::cout << c2.getType() << std::endl;
+//     std::cout << c3.getType() << std::endl;
+// }
