98971adfc8089d491871bd3e9f0aff00b085e291
zech-chi <machinelearning1.618@gmail.com>
2024-08-03 10:03:25 +0100

    cpp04/ex00

--- CPP_Module_04/ex00/Animal.hpp
+++ CPP_Module_04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 16:34:21 by zech-chi          #+#    #+#             */
-/*   Updated: 2024/08/02 19:09:25 by zech-chi         ###   ########.fr       */
+/*   Updated: 2024/08/03 09:04:32 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@
 class   Animal
 {
 
-    protected:
-        std::string type;
+	protected:
+		std::string type;
 
-    public:
-        Animal();
-        Animal( const Animal& origine );
-        Animal& operator=( const Animal& origine );
-        virtual ~Animal();
+	public:
+		Animal();
+		Animal( const Animal& origine );
+		Animal& operator=( const Animal& origine );
+		virtual ~Animal();
 
-        std::string    getType() const;
-        virtual void    makeSound() const;
+		std::string		getType() const;
+		virtual void	makeSound() const;
 
 };
