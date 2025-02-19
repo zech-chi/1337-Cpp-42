4f40f2cf4c0a57b260668923af6e27c238968b65
zech-chi <machinelearning1.618@gmail.com>
2025-02-19 10:50:46 +0100

    update Form

--- CPP_Module_05/ex02/AForm.hpp
+++ CPP_Module_05/ex02/AForm.hpp
@@ -18,7 +18,7 @@ class AForm {
         AForm(const std::string &name, int gradeToSign, int gradeToExecute);
         AForm(const AForm& other);
         AForm& operator = (const AForm& other);
-        ~AForm();
+        virtual ~AForm();
 
         const std::string& getName() const;
         bool getIsSigned() const;
