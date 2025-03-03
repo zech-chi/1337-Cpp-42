45836766d056153f13327855f051f5d2cc7fe955
zech-chi <machinelearning1.618@gmail.com>
2025-03-03 17:52:46 +0000

    .

---
+++ CPP_Module_08/ex02/MutantStack.hpp
@@ -0,0 +1,15 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+// Bold Text Color
+#define BOLD_RED "\033[1;31m"
+#define BOLD_GREEN "\033[1;32m"
+#define BOLD_BLUE "\033[1;34m"
+#define BOLD_YELLOW "\033[1;33m"
+
+// Reset Color
+#define RESET "\033[0m"
+
+#include <iostream>
+
+#endif
